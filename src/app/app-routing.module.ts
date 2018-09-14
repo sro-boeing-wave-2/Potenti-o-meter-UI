638498e1360434be6138bf84731acc7f024e5b4f
Varun Malik <varun1malik@gmail.com>
2018-09-14 14:13:41 +0530

    added changes for login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlayerComponent } from './player/player.component';
 import { StartQuizComponent } from './start-quiz/start-quiz.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '',redirectTo: '/start:id/:domain',  pathMatch: 'full'},
   {path:'start/:id/:domain', component:StartQuizComponent},
-  {path:'player/:id', component:PlayerComponent}
+  {path:'player/:id', component:PlayerComponent},
+  {  path: 'home', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,21 @@ import { FooterComponent } from './Shared/footer/footer.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import {MatToolbarModule, MatDialog, MatDialogModule} from '@angular/material';
+import {MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from './/app-routing.module';
 import { PlayerService } from './player.service';
 import {MaterialModule} from '../materialmodule';
-import { FormsModule } from '@angular/forms';
+import { MatMenuModule} from '@angular/material/menu';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatRadioModule} from '@angular/material/radio';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { DashboardBodyComponent } from './dashboard-body/dashboard-body.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { HomeComponent } from './home/home.component';
+import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.component';
 
 
 
@@ -26,7 +35,13 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     PlayerComponent,
     McqComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    UserLoginComponent,
+    HomeComponent,
+    UserSignUpComponent,
+    DashboardComponent,
+    DashboardHeaderComponent,
+    DashboardBodyComponent
   ],
   imports: [
     BrowserModule,
@@ -36,10 +51,17 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
+    MatToolbarModule,
     MatRadioModule,
+    FormsModule,
+    MatMenuModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatIconModule,
     MaterialModule
   ],
-  providers: [PlayerService],
-  bootstrap: [AppComponent]
+  providers: [PlayerService, MatDialog],
+  bootstrap: [AppComponent],
+  entryComponents: [UserLoginComponent,UserSignUpComponent],
 })
 export class AppModule { }
