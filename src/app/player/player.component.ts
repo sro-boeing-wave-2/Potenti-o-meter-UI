25476c1eeee4dbbd47bac4c0a790ed0063ca84df
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-10-01 14:40:02 +0530

    modified timer logic

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input, OnInit, ViewChild, ComponentFactoryResolver } from '@angular/core';
-import { QuestionDirective } from '../question.directive';
+import { QuestionDirective} from '../question.directive';
 import { AdComponents } from '../adComponent';
 import { QuestionModel } from '../questionModule';
 import { PlayerService } from '../player.service';
-import { McqComponent } from '../mcq/mcq.component';
+import { McqComponent} from '../mcq/mcq.component';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { AdItem } from '../ad-item';
 import { PlatformLocation } from '@angular/common'
@@ -21,19 +21,19 @@ export class PlayerComponent implements OnInit {
   @Input() questionComponents: AdItem[];
   @ViewChild(QuestionDirective) questionHost: QuestionDirective;
 
-  constructor(private componentFactoryResolver: ComponentFactoryResolver, private playerService: PlayerService, private activatedRoute: ActivatedRoute, private localStorage: LocalStorageService,
+  constructor(private componentFactoryResolver: ComponentFactoryResolver,private playerService: PlayerService, private activatedRoute: ActivatedRoute, private localStorage: LocalStorageService,
     private location: PlatformLocation, private router: Router) {
-    location.onPopState(() => {
-      var r = confirm("We detected a back button press. Do you want to submit the test ?");
-      if (r == true) {
-        this.endQuiz();
-      }
-      else {
-        window.location.reload();
-      }
+      location.onPopState(() => {
+        var r = confirm("We detected a back button press. Do you want to submit the test ?");
+        if (r == true) {
+          this.endQuiz();
+        }
+        else {
+          window.location.reload();
+        }
     });
-  }
-  res: MCQOption;
+   }
+   res: MCQOption;
   userId: number;
   domainName: string;
   timer: any = null;
@@ -41,7 +41,7 @@ export class PlayerComponent implements OnInit {
   endTime: Date;
   ellapsedTime = '00:00';
   duration = '';
-  questionType: string;
+  questionType : string;
   userResponse: any;
   count: number;
   progress: number;
@@ -50,11 +50,11 @@ export class PlayerComponent implements OnInit {
   public empty = false;
   ngOnInit() {
 
-    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+    this.activatedRoute.paramMap.subscribe((params: ParamMap)=> {
       let id = parseInt(params.get('id'));
       this.userId = id;
     });
-    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+    this.activatedRoute.paramMap.subscribe((params: ParamMap)=> {
       let domain = params.get('domain');
       this.domainName = domain;
     });
@@ -68,16 +68,16 @@ export class PlayerComponent implements OnInit {
         },
         error => console.log(error)
       );
-    this.count = 1;
-    this.progress = 10;
-    this.questionComponents = this.playerService.getComponents();
-    this.startTime = new Date();
-    this.timer = setInterval(() => { this.tick(); }, 1000);
-    this.duration = this.parseTime(300);
+     this.count = 1;
+     this.progress = 10;
+     this.questionComponents = this.playerService.getComponents();
+     this.startTime = new Date();
+     this.timer = setInterval(() => { this.tick(); }, 1000);
+     this.duration = this.parseTime(300);
   }
-  question: any;
-  mcqQuestion: MCQModel;
-  mmcqQuestion: MMCQModel;
+  question : any;
+  mcqQuestion : MCQModel;
+  mmcqQuestion : MMCQModel;
   onResponseReceived(response) {
     this.userResponse = response;
   }
@@ -86,25 +86,25 @@ export class PlayerComponent implements OnInit {
     this.questionType = this.question["questionType"];
     this.count = this.count + 1;
     this.progress = this.progress + 10;
-    switch (this.questionType) {
+    switch(this.questionType) {
       case "MCQ":
-        {
-          this.res = {
-            raw: this.localStorage.retrieve("response"),
-            optionText: this.localStorage.retrieve("response")
-          };
-          this.mcqQuestion.response = this.res;
+      {
+        this.res = {
+          raw: this.localStorage.retrieve("response"),
+          optionText: this.localStorage.retrieve("response")
+        };
+        this.mcqQuestion.response = this.res;
 
 
 
-          return this.playerService.getNextQuestion(this.mcqQuestion);
-        }
+      return this.playerService.getNextQuestion(this.mcqQuestion);
+      }
 
       case "MMCQ":
-        {
-          this.mmcqQuestion.response = this.localStorage.retrieve("response");
-          return this.playerService.getNextQuestion(this.mmcqQuestion);
-        }
+      {
+      this.mmcqQuestion.response = this.localStorage.retrieve("response");
+      return this.playerService.getNextQuestion(this.mmcqQuestion);
+      }
     }
 
 
@@ -113,85 +113,85 @@ export class PlayerComponent implements OnInit {
 
   endQuiz() {
     this.questionType = this.question["questionType"];
-    switch (this.questionType) {
+    switch(this.questionType) {
       case "MCQ":
-        {
-          this.res = {
-            raw: this.localStorage.retrieve("response"),
-            optionText: null
-          };
-          this.mcqQuestion.response = this.res;
-          return this.playerService.endQuiz(this.mcqQuestion);
-        }
+      {
+        this.res = {
+          raw: this.localStorage.retrieve("response"),
+          optionText: null
+        };
+        this.mcqQuestion.response = this.res;
+      return this.playerService.endQuiz(this.mcqQuestion);
+      }
 
       case "MMCQ":
-        {
-          this.mmcqQuestion.response = this.localStorage.retrieve("response");
-          return this.playerService.endQuiz(this.mmcqQuestion);
-        }
+      {
+      this.mmcqQuestion.response = this.localStorage.retrieve("response");
+      return this.playerService.endQuiz(this.mmcqQuestion);
+      }
     }
 
 
   }
   tick() {
-    const now = new Date();
-    const diff = (now.getTime() - this.startTime.getTime()) / 1000;
-    if (diff >= 300 && diff < 301) {
+      const now = new Date();
+      const diff = (now.getTime() - this.startTime.getTime()) / 1000;
+      // if (diff >= 300 && diff< 301) {
+      //   this.endQuiz();
+      // }
       this.ellapsedTime = this.parseTime(diff);
-      const endTime = this.ellapsedTime.split(":", 2);
+      const endTime = this.ellapsedTime.split(":",2);
       const minutes = +endTime[0];
-      this.endQuiz();
-
-      // this.ellapsedTime = this.parseTime(diff);
-      // const endTime = this.ellapsedTime.split(":",2);
-      // const minutes = +endTime[0];
-      if (minutes > 4) {
-        this.half = true;
-        this.full = false;
-      }
-      if (minutes > 7) {
-        this.half = false;
-        this.empty = true;
-      }
+    if(minutes > 2)
+    {
+      this.half= true;
+      this.full = false;
     }
-  }
+    if(minutes > 3){
+      this.half = false;
+      this.empty= true;
+    }if(minutes == 5){
+      this.endQuiz();
+    }
 
-  parseTime(totalSeconds: number) {
-    let mins: string | number = Math.floor(totalSeconds / 60);
-    let secs: string | number = Math.round(totalSeconds % 60);
-    mins = (mins < 10 ? '0' : '') + mins;
-    secs = (secs < 10 ? '0' : '') + secs;
-    return `${mins}:${secs}`;
-  }
-  loadComponent(question: any) {
+    }
+
+    parseTime(totalSeconds: number) {
+      let mins: string | number = Math.floor(totalSeconds / 60);
+      let secs: string | number = Math.round(totalSeconds % 60);
+      mins = (mins < 10 ? '0' : '') + mins;
+      secs = (secs < 10 ? '0' : '') + secs;
+      return `${mins}:${secs}`;
+    }
+  loadComponent(question:any) {
 
 
     let adItem;
     this.questionType = question["questionType"];
     console.log("INSIDE LOAD COMPONENT " + this.questionType);
-    switch (this.questionType) {
+    switch(this.questionType) {
       case "MCQ":
-        {
-          this.mcqQuestion = question;
-          adItem = this.questionComponents[0];
-          let componentFactory = this.componentFactoryResolver.resolveComponentFactory(adItem.component);
-          let viewContainerRef = this.questionHost.viewContainerRef;
-          viewContainerRef.clear();
-          let componentRef = viewContainerRef.createComponent(componentFactory);
-          (<AdComponents>componentRef.instance).question = this.mcqQuestion;
-        }
-        break;
+      {
+      this.mcqQuestion = question;
+      adItem = this.questionComponents[0];
+      let componentFactory = this.componentFactoryResolver.resolveComponentFactory(adItem.component);
+      let viewContainerRef = this.questionHost.viewContainerRef;
+      viewContainerRef.clear();
+      let componentRef = viewContainerRef.createComponent(componentFactory);
+      (<AdComponents>componentRef.instance).question = this.mcqQuestion;
+      }
+      break;
       case "MMCQ":
-        {
-          this.mmcqQuestion = question;
-          adItem = this.questionComponents[0];
-          let componentFactory = this.componentFactoryResolver.resolveComponentFactory(adItem.component);
-          let viewContainerRef = this.questionHost.viewContainerRef;
-          viewContainerRef.clear();
-          let componentRef = viewContainerRef.createComponent(componentFactory);
-          (<AdComponents>componentRef.instance).question = this.mmcqQuestion;
-        }
-        break;
+      {
+      this.mmcqQuestion = question;
+      adItem = this.questionComponents[0];
+      let componentFactory = this.componentFactoryResolver.resolveComponentFactory(adItem.component);
+      let viewContainerRef = this.questionHost.viewContainerRef;
+      viewContainerRef.clear();
+      let componentRef = viewContainerRef.createComponent(componentFactory);
+      (<AdComponents>componentRef.instance).question = this.mmcqQuestion;
+      }
+      break;
 
     }
     // if(question.questionType == "MCQType")
