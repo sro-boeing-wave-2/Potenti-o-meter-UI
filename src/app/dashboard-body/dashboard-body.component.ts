0f104a9a475c4493903265ce0f8ef8c9031779e3
Varun Malik <varun1malik@gmail.com>
2018-10-01 11:55:17 +0530

    removed dummy code

--- src/app/dashboard-body/dashboard-body.component.ts
+++ src/app/dashboard-body/dashboard-body.component.ts
@@ -35,6 +35,7 @@ export class DashboardBodyComponent implements OnInit {
     {"color":"blue","tagline":"Know the reason behind every fiction","icon":"nature_people"},
     {"color":"pink","tagline":"Java: write once, run away!","icon":"code"},
     {"color":"red","tagline":"Know the reason behind every fiction","icon":"nature_people"},
+    {"color":"green","tagline":"An algorithm must be seen to be believed","icon":"code"},
   ]
 
 }
