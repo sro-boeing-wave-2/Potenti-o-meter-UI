86127da044cbd00c176d1bd2e3b165fe99f011fc
Varun Malik <varun1malik@gmail.com>
2018-10-03 17:05:13 +0530

    sumitted test

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -59,22 +59,19 @@ export class PlayerComponent implements OnInit {
 
   outFocus() {
     if(!document.hasFocus()){
-      if(PlayerComponent.outofFocus < 1 && this.dialogRef==null) {
+      if(this.dialogRef==null){
       this.dialogRef = this.dialog.open(SubmitWarning, {
       });
-      }
       this.dialogRef.afterClosed().subscribe(result => {
-        this.dialogRef ==null;
+        this.dialogRef == null;
         PlayerComponent.outofFocus++;
-        console.log("value of out of focus is " +PlayerComponent.outofFocus);
+        if(PlayerComponent.outofFocus >= 2)
+        {
+          this.endQuiz();
+        }
       });
-
-      if(PlayerComponent.outofFocus >= 1)
-      {
-      console.log("ending quiz now");
-      this.endQuiz();
-      }
     }
+  }
   }
 
   ngOnInit() {
