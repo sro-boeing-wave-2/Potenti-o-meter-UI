c0db6e8bf1ae6c9eeddeb5f75b383811fa8806d3
Varun Malik <varun1malik@gmail.com>
2018-10-05 01:17:08 +0530

    replaced get call from ngonviewchecked to ngoninit

--- src/app/recommendations/recommendations.component.ts
+++ src/app/recommendations/recommendations.component.ts
@@ -8,7 +8,7 @@ import { SignUpService } from '../sign-up.service';
   templateUrl: './recommendations.component.html',
   styleUrls: ['./recommendations.component.css']
 })
-export class RecommendationsComponent implements OnInit, AfterViewChecked {
+export class RecommendationsComponent implements OnInit {
 
   constructor(private router: Router, private dashboardsservice: DashboardService) { }
   @Input() domain;
@@ -16,9 +16,6 @@ export class RecommendationsComponent implements OnInit, AfterViewChecked {
   @Output() dashbaord = new EventEmitter();
   public RecommendationData = []
   ngOnInit() {
-  }
-
-  ngAfterViewChecked() {
     this.dashboardsservice.getRecommendations(this.UserData.UserID,this.domain).subscribe(result =>
       this.RecommendationData.push(...result.json()));
     console.log(this.RecommendationData);
